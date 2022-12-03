625c2b7b0906e80c1a2f5328fb6687b308ee07c3
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-12-03 23:20:14 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const driver = [
+    {
+        firstName: "James",
+        homeTown:"Westlands"
+    },
+    {
+        firstName:"Mike",
+        homeTown:"Dagoretti"
+    }
+]
+
+function firstNamePrinter(collection){
+    for(const driver of collection){
+        console.log(driver.firstName);
+        return driver;
+    }
+}
